187a8fcbd477caa6c0113df40eec40fc1097951e
johnnyFR26 <149261007+johnnyFR26@users.noreply.github.com>
2025-04-16 09:13:50 -0300

    return categories to login

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'api-thinktech',
+        script: 'npm start',
+        combine_logs: true,
+        time: true,
+        max_memory_restart: '1G',
+      },
+    ],
+  }
