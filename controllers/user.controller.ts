e9d5507866cea772b5369bd8541628103b8027c3
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-05-28 22:14:55 -0300

    adiciona ferramentas de depuracao de velocidade e desempenho

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -56,13 +56,11 @@ export class UserController {
           
         const users = await db.user.findMany()
 
-        await redisClient.setEx(cacheKey, 30, JSON.stringify(users))
+        await redisClient.setEx(cacheKey, 50, JSON.stringify(users))
 
         return reply.status(200).send(users)
     }
-
-
-
+ 
 
     /**
      * Retrieves a user by email.
