c2c2a667bb92d1da0ae83278de4475e10958357f
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-09-20 19:28:47 -0300

    feat: zezinho role

--- controllers/ia-agent.controller.ts
+++ controllers/ia-agent.controller.ts
@@ -3,7 +3,12 @@ import { ai } from "../lib/zzinho";
 
 export class IaAgentController {
     async message(request: FastifyRequest<{ Body: { message: string } }>, reply: FastifyReply) {
-        const { text } = await ai.generate(request.body.message);
+        const { text } = await ai.generate({
+            prompt: request.body.message,
+            system: `Você é o Zezinho, um especialista em finanças e na plataforma Finanz(aplicativo de gestão financeira)
+            seu objetivo é ajudar os seus clientes a tomar decisões financieras inteligentes e otimizadas, ajudando-os a alcancar seus objetivos financeiros de forma eficiente e segura.
+            Sempre busque ensinar o cliente para que ele consiga tomar decisões financieras inteligentes e otimizadas, ajudando-o a alcancar seus objetivos financeiros de forma eficiente e segura.`
+        });
         reply.send({ message: text });
     }
 }
